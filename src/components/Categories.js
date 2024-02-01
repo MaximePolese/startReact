c0d97fed48dac57e4f8b9be274c798b9296cbb77
Maxime Polese <maxime.polese@le-campus-numerique.fr>
2024-02-01 10:19:03 +0100

    Selection des plantes par catégories

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,9 +1,9 @@
 import {plantList} from "../datas/plantList";
+import '../styles/Categories.css'
 
-const Categories = () => {
+const Categories = ({selectedCategory, setSelectedCategory}) => {
 
     const categories = [];
-
     plantList.forEach(function (plant) {
         if (!categories.includes(plant.category)) {
             categories.push(plant.category);
@@ -11,9 +11,15 @@ const Categories = () => {
     })
 
     return (
-        <select>
-            {categories.map((category) => (
-                <option key={category}>{category}</option>
-            ))}
-        </select>)
+        <div className="categories">
+            <label htmlFor="select">Choisir une catégorie : </label>
+            <select id="select" value={selectedCategory} onChange={(e) => setSelectedCategory(e.target.value)}>
+                <option value="">toutes les plantes</option>
+                {categories.map((category) => (
+                    <option key={category} value={category}>{category}</option>
+                ))}
+            </select>
+        </div>)
 }
+
+export default Categories
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,15 +1,21 @@
+import {useState} from 'react'
 import {plantList} from '../datas/plantList'
 import '../styles/ShoppingList.css'
 import PlantItem from './PlantItem'
 import Categories from './Categories'
 
 function ShoppingList({cart, updateCart}) {
+    const [selectedCategory, setSelectedCategory] = useState("");
+    const filteredPlantList = selectedCategory ? plantList.filter(plant => plant.category === selectedCategory) : plantList;
 
     return (
         <div className='lmj-shopping-list'>
-            <Categories/>
+            <Categories
+                selectedCategory={selectedCategory}
+                setSelectedCategory={setSelectedCategory}
+            />
             <ul className='lmj-plant-list'>
-                {plantList.map(({id, cover, name, water, light, isBestSale, isSpecialOffer, price}) => (
+                {filteredPlantList.map(({id, cover, name, water, light, isBestSale, isSpecialOffer, price}) => (
                     <PlantItem
                         id={id}
                         cover={cover}
