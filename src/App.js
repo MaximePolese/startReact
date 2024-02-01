3775ae9f3e9f2d1e58d87cb7a2ef9a41bbd2cc57
Maxime Polese <maxime.polese@le-campus-numerique.fr>
2024-02-01 17:06:22 +0100

    todo fonctionnalités dans App.js

--- src/App.js
+++ src/App.js
@@ -23,3 +23,10 @@ function App() {
 }
 
 export default App;
+
+// Ajoutez des fonctionnalités
+// Vous avez peut-être envie de pousser notre site La maison jungle plus loin ? Voilà quelques idées de features qui vous permettraient de vous exercer :
+//     Permettre de supprimer des articles dans le panier.
+//     Permettre de sélectionner plusieurs catégories.
+//     Afficher des informations supplémentaires au clic sur un article dans une modale.
+//     Ajouter du style : des animations pour ouvrir/fermer le panier, sur le header, personnaliser les boutons, etc.
